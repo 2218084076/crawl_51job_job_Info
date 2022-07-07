9aa0d1503df89efe188cabe327f3f1483bfd0600
Terry <82017869+2218084076@users.noreply.github.com>
2022-07-07 10:54:31 +0800

    requests 方法请求51job

---
+++ demo_re.py
@@ -0,0 +1,24 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+}
+url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,%25E5%25A4%25A7%25E6%2595%25B0%25E6%258D%25AE,2,1.html'
+try:
+    response = requests.get(
+        url,
+        headers=headers,
+        verify=False
+    )
+    print('a')
+except Exception as ex:
+    print(ex)
+
+
+class Crawl:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    }
+    url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,%25E5%25A4%25A7%25E6%2595%25B0%25E6%258D%25AE,2,1.html'
+
+
